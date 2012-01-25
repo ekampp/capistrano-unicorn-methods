b9829aa1c3d1b8d6aa773da501b14df5ea644081
Emil Kampp <emiltk@benjamin.dk>
2012-01-25 14:17:55 +0100

    Updated load method

--- lib/capistrano-unicorn-methods.rb
+++ lib/capistrano-unicorn-methods.rb
@@ -2,50 +2,48 @@ require "capistrano-unicorn-methods/version"
 
 module CapistranoUnicornMethods
   def self.load
-    Capistrano::Configuration.instance(true).load do
-      namespace :unicorn do
+    namespace :unicorn do
 
-        # Lazy setting these variables, as they (might) depend on other settings
-        set(:unicorn_pid)     { "#{current_path}/tmp/pids/unicorn.pid" }
-        set(:unicorn_old_pid) { "#{current_path}/tmp/pids/unicorn.pid.oldbin" }
-        set(:unicorn_config)  { "#{current_path}/config/unicorn.rb" }
-        set(:unicorn_port)    { 3000 }
-        set(:use_bundler)     { true }
-        set(:rails_env)       { "production" }
+      # Lazy setting these variables, as they (might) depend on other settings
+      set(:unicorn_pid)     { "#{current_path}/tmp/pids/unicorn.pid" }
+      set(:unicorn_old_pid) { "#{current_path}/tmp/pids/unicorn.pid.oldbin" }
+      set(:unicorn_config)  { "#{current_path}/config/unicorn.rb" }
+      set(:unicorn_port)    { 3000 }
+      set(:use_bundler)     { true }
+      set(:rails_env)       { "production" }
 
-        desc "Zero-downtime restart of Unicorn"
-        task :restart do
-          unicorn.cleanup
-          run "touch #{unicorn_pid}"
-          pid = capture("cat #{unicorn_pid}").to_i
-          run "kill -s USR2 #{pid}" if pid > 0
-        end
+      desc "Zero-downtime restart of Unicorn"
+      task :restart do
+        unicorn.cleanup
+        run "touch #{unicorn_pid}"
+        pid = capture("cat #{unicorn_pid}").to_i
+        run "kill -s USR2 #{pid}" if pid > 0
+      end
 
-        desc "Starts unicorn"
-        task :start do
-          unicorn.cleanup
-          run "cd #{current_path} ; #{'bundle exec' if use_bundler} unicorn_rails -c #{unicorn_config} -D -p #{unicorn_port} -E #{rails_env}"
-        end
+      desc "Starts unicorn"
+      task :start do
+        unicorn.cleanup
+        run "cd #{current_path} ; #{'bundle exec' if use_bundler} unicorn_rails -c #{unicorn_config} -D -p #{unicorn_port} -E #{rails_env}"
+      end
 
-        desc "Stop unicorn"
-        task :stop do
-          run "touch #{unicorn_pid}"
-          pid = capture("cat #{unicorn_pid}").to_i
-          run "kill -s QUIT #{pid}" if pid > 0
-        end
+      desc "Stop unicorn"
+      task :stop do
+        run "touch #{unicorn_pid}"
+        pid = capture("cat #{unicorn_pid}").to_i
+        run "kill -s QUIT #{pid}" if pid > 0
+      end
 
-        desc "Cleans up the old unicorn processes"
-        task :cleanup do
-          run "touch #{unicorn_old_pid}"
-          pid = capture("cat #{unicorn_old_pid}").to_i
-          run "kill -s QUIT #{pid}" if pid > 0
-          ensure_writable_dirs
-        end
+      desc "Cleans up the old unicorn processes"
+      task :cleanup do
+        run "touch #{unicorn_old_pid}"
+        pid = capture("cat #{unicorn_old_pid}").to_i
+        run "kill -s QUIT #{pid}" if pid > 0
+        ensure_writable_dirs
+      end
 
-        task :ensure_writable_dirs do
-          dir = File.dirname(unicorn_pid)
-          run "chmod a+w #{dir}"
-        end
+      task :ensure_writable_dirs do
+        dir = File.dirname(unicorn_pid)
+        run "chmod a+w #{dir}"
       end
     end
   end
