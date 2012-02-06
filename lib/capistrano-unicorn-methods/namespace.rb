2c41a33b6287ebcc75ff29cc824ee4d8e4bbe0f6
Emil Kampp <emiltk@benjamin.dk>
2012-02-06 12:05:24 +0100

    Updated internal documentation

--- lib/capistrano-unicorn-methods/namespace.rb
+++ lib/capistrano-unicorn-methods/namespace.rb
@@ -9,6 +9,10 @@ Capistrano::Configuration.instance.load do
     set(:use_bundler)     { true }
     set(:rails_env)       { "production" }
 
+    #
+    # Restarts the unicorn process(es) with the URS2 code, to gracefully
+    # create a new server, and kill of the old one, leaving *no* downtime
+    #
     desc "Zero-downtime restart of Unicorn"
     task :restart do
       unicorn.cleanup
@@ -17,6 +21,9 @@ Capistrano::Configuration.instance.load do
       run "kill -s USR2 #{pid}" if pid > 0
     end
 
+    #
+    # Starts the unicorn process(es)
+    #
     desc "Starts unicorn"
     task :start, :roles => :web do
       logger.info "Starting unicorn server(s).."
@@ -24,6 +31,12 @@ Capistrano::Configuration.instance.load do
       run "cd #{current_path} ; #{'bundle exec' if use_bundler} unicorn_rails -c #{unicorn_config} -D -p #{unicorn_port} -E #{rails_env}"
     end
 
+    #
+    # This will quit the unicorn process(es).
+    #
+    # NB! This will not restart the process, to the server(s) will effectively
+    # be down!
+    #
     desc "Stop unicorn"
     task :stop, :roles => :web do
       logger.info "Stopping unicorn server(s).."
@@ -32,6 +45,10 @@ Capistrano::Configuration.instance.load do
       run "kill -s QUIT #{pid}" if pid > 0
     end
 
+    #
+    # This will clean up any old unicorn servers left behind by the USR2 kill
+    # command.
+    #
     desc "Cleans up the old unicorn processes"
     task :cleanup, :roles => :web do
       logger.info "Cleaning out old unicorn server(s).."
@@ -41,6 +58,12 @@ Capistrano::Configuration.instance.load do
       ensure_writable_dirs
     end
 
+    #
+    # This will mod the pid dirs to ensure that future pid files can be written
+    # to, and read from the dirs.
+    #
+    # NB! This is only intented for internal usage
+    #
     task :ensure_writable_dirs, :role => :web do
       dir = File.dirname(unicorn_pid)
       run "chmod a+w #{dir}"
